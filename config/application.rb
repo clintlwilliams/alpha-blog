bdf0df83c5567f7a59214ed6ed30701069837100
Rails Udemy Course <cwilliams@gracehill.com>
2020-02-11 14:21:00 -0500

    remove active_job/railtie

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require "rails"
 
 # Include each railties manually, excluding `active_storage/engine`
 # require "active_model/railtie"
-require "active_job/railtie"
+# require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
